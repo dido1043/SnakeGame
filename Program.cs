6f57f32fabcbf67597d34f860594f8da7f5a1d96
dido1043 <80157675+dido1043@users.noreply.github.com>
2023-01-21 23:06:51 +0200

    Up

--- Program.cs
+++ Program.cs
@@ -1,33 +1,54 @@
-﻿/*Console.WriteLine("Do you want play? \n (Y/N)");
+﻿Console.WriteLine("Do you want play? \n (Y/N)");
 
 var response = Console.ReadLine();
 if (response == "N" || response != "Y")
     return;
-*/
-char ch = '*';
+
+
+Console.WriteLine();
+Console.WriteLine();
 bool gameLive = true;
 ConsoleKeyInfo consoleKey; // holds whatever key is pressed
 
 int x = 0;
-int y = 2;
+int y = 4;
 
 int dx = 1;
 int dy = 0;
+
+int consoleWidthLimit = 79;
+int consoleHeightLimit = 24;
 
 int delay = 50;
 
 while (gameLive)
 {
+    Console.Write("");
     Console.SetCursorPosition(0, 0);
     Console.WriteLine("Arrows move up/down/right/left.,  'c' is for clear,  'esc'is for quit.");
     Console.SetCursorPosition(x, y);
+
 
     if (Console.KeyAvailable)
     {
         consoleKey = Console.ReadKey(true);
 
-        switch (consoleKey.Key)
+        switch (consoleKey.Key)            
         {
+            //Colors
+            case ConsoleKey.R:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case ConsoleKey.G:
+                Console.ForegroundColor = ConsoleColor.Gray;
+                break;
+            case ConsoleKey.Y:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case ConsoleKey.B:
+                Console.ForegroundColor = ConsoleColor.Blue;
+                break;
+                //Movings
             case ConsoleKey.UpArrow:
                 dx = 0;
                 dy = -1;
@@ -52,5 +73,20 @@ while (gameLive)
                 break;
         }
     }
+    Console.SetCursorPosition(x, y);
+    x += dx;
+    if (x > consoleWidthLimit)
+        x = 0;
+    if (x < 0)
+        x = consoleWidthLimit;
 
+    y += dy;
+    if (y > consoleHeightLimit)
+        y = 4;
+    if (y < 4)
+        y = consoleHeightLimit;
+
+    Console.SetCursorPosition(x, y);
+    Console.Write('*');
+    Thread.Sleep(delay);
 }
